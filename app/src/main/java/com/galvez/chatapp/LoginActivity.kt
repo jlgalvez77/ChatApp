448c498939f5be957a794dd65b858bf92727a66f
jlgalvez77 <jlgalvez77@gmail.com>
2023-09-16 22:09:45 +0200

    login

--- app/src/main/java/com/galvez/chatapp/LoginActivity.kt
+++ app/src/main/java/com/galvez/chatapp/LoginActivity.kt
@@ -1,11 +1,62 @@
 package com.galvez.chatapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var L_Et_email : EditText
+    private lateinit var L_Et_password : EditText
+    private lateinit var Btn_login : Button
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login2)
+        setContentView(R.layout.activity_login)
+        supportActionBar!!.title = "Login"
+        InicializarVariables()
+        Btn_login.setOnClickListener(){
+            ValidarDatos()
+        }
+    }
+    private fun InicializarVariables(){
+        L_Et_email = findViewById(R.id.L_Et_email)
+        L_Et_password = findViewById(R.id.L_Et_password)
+        Btn_login = findViewById(R.id.Btn_login)
+        auth = FirebaseAuth.getInstance()
+    }
+    private fun ValidarDatos() {
+        val email : String = L_Et_email.text.toString()
+        val password : String = L_Et_password.text.toString()
+
+        if(email.isEmpty()){
+            Toast.makeText(applicationContext, "Introduce tu dirección de correo electrónico", Toast.LENGTH_SHORT).show()
+        }
+        if(password.isEmpty()){
+            Toast.makeText(applicationContext, "Introduce la contraseña", Toast.LENGTH_SHORT).show()
+        }
+        else {
+            LoginUsuario(email, password)
+        }
+    }
+
+    private fun LoginUsuario(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(){task->
+            if(task.isSuccessful){
+                val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                Toast.makeText(applicationContext, "Ha iniciado sesión", Toast.LENGTH_SHORT).show()
+                startActivity(intent)
+                finish()
+            }else{
+                Toast.makeText(applicationContext, "Ha ocurrido un error", Toast.LENGTH_SHORT).show()
+            }
+        }.addOnFailureListener {e->
+            Toast.makeText(applicationContext, "{${e.message}}", Toast.LENGTH_SHORT).show()
+        }
     }
 }
